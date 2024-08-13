2f9760a0a3f58423325850658f7316dabae4c1ad
autoplay1999 <autoplay1999@hotmail.com>
2024-08-13 22:46:01 +0700

    เพิ่ม manifest

--- phlib/include/ph.h
+++ phlib/include/ph.h
@@ -10,6 +10,8 @@
  *
  */
 
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #ifndef _PH_PH_H
 #define _PH_PH_H
 
