7a74922ed8813366873bc24fe7386e0728aae93b
autoplay1999 <autoplay1999@hotmail.com>
2024-08-14 00:58:01 +0700

    เพิ่มออโต้ lib ntdll, winsta

--- phlib/include/ph.h
+++ phlib/include/ph.h
@@ -11,6 +11,8 @@
  */
 
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "ntdll.lib")
+#pragma comment(lib, "Winsta.lib")
 
 #ifndef _PH_PH_H
 #define _PH_PH_H
